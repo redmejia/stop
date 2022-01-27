9d121c1827bbdf2370f90837b7f733ff25926eca
redmejia <preynaldorod@gmail.com>
2022-01-26 21:36:16 -0800

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Main from './Components/Main';
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,28 @@
+function Footer() {
+	return (
+		<>
+			<footer className="bg-dark mt-2">
+				<div className="container pt-4">
+					<div className="row text-danger">
+						<div className="col-sm-4 text-center">
+							<p>home</p>
+						</div>
+						<div className="col-sm-4 text-center">
+							<h5>Social</h5>
+							<p>Facebook</p>
+							<p>Instagram</p>
+							<p>Twitter</p>
+						</div>
+						<div className="col-sm-4 text-center">
+							<h5>Contact</h5>
+							<p>123 23456678</p>
+							<p>stop@mail.com</p>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
+}
+
+export default Footer;
